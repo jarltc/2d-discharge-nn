e3966e8d1b6d88a8425a43034a91c61709f62d38
chao-zu <jcollado@observatory.ph>
2022-12-07 10:41:23 +0900

    Converted param_array to list comprehension

--- data_augmentation.py
+++ data_augmentation.py
@@ -46,6 +46,38 @@ def create_dummy_data_table(data_dir, voltage, pressure):
     return dummy_df
 
 
+def interpolation(parameter, interpolation_variable=None):
+    """ Interpolate between two arrays
+
+    Args:
+        parameter (str): Column name for parameter to interpolate.
+        interpolation_variable (str, optional): Interpolate across voltage or pressure. Defaults to None.
+
+    Raises:
+        Exception: Raised when interpolation variable is not specified.
+
+    Returns:
+        pd.Series: pd.Series of new column created from interpolation.
+    """
+    if interpolation_variable == 'voltage':
+        start_variable = start_voltage
+        end_variable = end_voltage
+        mid_variable = inter_voltage
+    elif interpolation_variable == 'pressure':
+        start_variable = start_pressure
+        end_variable = end_pressure
+        mid_variable = inter_pressure
+    else: 
+        raise Exception('Interpolation variable not specified!')
+
+    start_array = start_df[parameter].values
+    end_array = end_df[parameter].values
+    interpolator = interp1d([start_variable, end_variable], np.vstack([start_array, end_array]), axis=0)
+    inter_array = pd.Series(interpolator(mid_variable), name=parameter)
+    
+    return inter_array
+
+
 data_folder = Path('/Users/jarl/2d-discharge-nn/data/avg_data')
 
 voltages = [200, 300, 400, 500]
@@ -64,26 +96,21 @@ for pressure in tqdm(pressures, desc=' pressure', position=0):
         start_df = get_data_table(data_folder, start_voltage, pressure)
         end_df = get_data_table(data_folder, end_voltage, pressure)
         parameters = list(start_df.columns[4:])
-        voltage = (start_voltage + end_voltage)/2
+        inter_voltage = (start_voltage + end_voltage)/2
 
+        # ensure X and Y are the same across both files
         assert start_df.iloc[:,2:4].all(axis=None) == end_df.iloc[:,2:4].all(axis=None)
 
-        parameter_arrays = []
-        for parameter in parameters:
-            start_array = start_df[parameter].values
-            end_array = end_df[parameter].values
-            interpolator = interp1d([start_voltage, end_voltage], np.vstack([start_array, end_array]), axis=0)
-            inter_array = pd.Series(interpolator(voltage), name=parameter)
-            parameter_arrays.append(inter_array)
+        parameter_arrays = [interpolation(parameter, 'voltage') for parameter in parameters]
 
         inter_df = pd.concat(parameter_arrays, axis=1)
         inter_df = pd.concat([inter_df, start_df.iloc[:,2:4]], axis=1)
-        inter_df['Vpp [V]'] = voltage
+        inter_df['Vpp [V]'] = inter_voltage
         inter_df['P [Pa]'] = pressure
 
         assert (start_df.columns == end_df.columns).all()
-        # inter_df[list(start_df.columns)].to_csv(out_dir / f'{voltage:.0f}V{pressure:.0f}Pa.csv', index=False)
-        inter_df[list(start_df.columns)].to_feather(feather_outdir / f'{voltage:.0f}Vpp{pressure:03.0f}Pa_node.feather')
+        # inter_df[list(start_df.columns)].to_csv(out_dir / f'{voltage:.0f}V{pressure:.0f}Pa.csv', index=False) # csv
+        inter_df[list(start_df.columns)].to_feather(feather_outdir / f'{inter_voltage:.0f}Vpp{pressure:03.0f}Pa_node.feather')
 
 # iterate over intermediate pressures
 for voltage in tqdm(voltages, desc='voltage'):
@@ -91,23 +118,17 @@ for voltage in tqdm(voltages, desc='voltage'):
         start_df = get_data_table(data_folder, voltage, start_pressure)
         end_df = get_data_table(data_folder, voltage, end_pressure)
         parameters = list(start_df.columns[4:])
-        pressure = (start_pressure + end_pressure)/2
+        inter_pressure = (start_pressure + end_pressure)/2
 
         assert start_df.iloc[:,2:4].all(axis=None) == end_df.iloc[:,2:4].all(axis=None)
 
-        parameter_arrays = []
-        for parameter in parameters:
-            start_array = start_df[parameter].values
-            end_array = end_df[parameter].values
-            interpolator = interp1d([start_pressure, end_pressure], np.vstack([start_array, end_array]), axis=0)
-            inter_array = pd.Series(interpolator(pressure), name=parameter)
-            parameter_arrays.append(inter_array)
+        parameter_arrays = [interpolation(parameter, 'pressure') for parameter in parameters]
 
         inter_df = pd.concat(parameter_arrays, axis=1)
         inter_df = pd.concat([inter_df, start_df.iloc[:,2:4]], axis=1)
         inter_df['Vpp [V]'] = voltage
-        inter_df['P [Pa]'] = pressure
+        inter_df['P [Pa]'] = inter_pressure
 
         assert (start_df.columns == end_df.columns).all()
         # inter_df[list(start_df.columns)].to_csv(out_dir / f'{voltage:.0f}V{pressure:.0f}Pa.csv', index=False)
-        inter_df[list(start_df.columns)].to_feather(feather_outdir / f'{voltage:.0f}Vpp{pressure:03.0f}Pa_node.feather')
+        inter_df[list(start_df.columns)].to_feather(feather_outdir / f'{voltage:.0f}Vpp{inter_pressure:03.0f}Pa_node.feather')
