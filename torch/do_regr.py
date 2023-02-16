Fixed scaling and unscaling functions

torch/do_regr.py
@@ -50,51 +50,87 @@ class MLP(nn.Module):
         return output
 
 
-def scale_features(data_table: pd.DataFrame, model_dir: Path):
+def scale_features(df: pd.DataFrame, model_dir: Path):
     """Scale table's features for regression.
 
     Args:
-        data_table (pd.DataFrame): Table of features to scale.
+        df (pd.DataFrame): Table of features to scale.
         model_dir (Path): Model directory to get the scaler files.
 
     Returns:
         torch.FloatTensor: Tensor of features that can be directly used for
         model evaluation.
     """
-    def scale_features_core(n, column):
-        column_values = data_table[column].values.reshape(-1, 1)
+    def scale_features_core(values, n):
+        # column_values = df[column].values.reshape(-1, 1)
+        values = np.array([values]).reshape(1, -1)
         scaler_file = model_dir/'scalers'/f'xscaler_{n:02}.pkl'
         with open(scaler_file, 'rb') as sf:
             xscaler = pickle.load(sf)
             xscaler.clip = False
-            return xscaler.transform(column_values)
+            return xscaler.transform(values).item()
 
-    columns = [scale_features_core(n, column) for n, column in enumerate(data_table.columns, start=1)]
-    scaled_df = pd.DataFrame(np.hstack(columns))
+    columns = [df[column].apply(scale_features_core, args=(n,)) for n, column in enumerate(df.columns, start=1)]
+    scaled_df = pd.concat(columns, axis=1)
+
+    # columns = [scale_features_core(n, column) for n, column in enumerate(data_table.columns, start=1)]
+    # scaled_df = pd.DataFrame(np.hstack(columns), columns=data_table.columns)
 
     return torch.FloatTensor(scaled_df.to_numpy())
 
 
-def scale_labels(data_table: pd.DataFrame, model_dir: Path, lin=True):
-    """Scale table's labels for regression.
+def scale_targets(data_table: pd.DataFrame, label_names: list, scale_exp=[1.0, 14.0, 14.0, 16.0, 0.0]):
+    """Scale target data (from simulation results).
+
+    Target data is in original scaling, whereas the training predictions are
+    scaled down by 10^n. Exponents used to scale the data down for 
+    training (n) are stored as a pickle file in the model folder. 
+    The pickle file is a list of powers, which are used to reverse the scaling.
 
     Args:
-        data_table (pd.DataFrame): Table of labels to scale.
-        model_dir (Path): Model directory to get the scaler files.
-        lin (bool, optional): Scale inputs linearly. Defaults to True.
+        data_table (pd.DataFrame): DataFrame of target data.
+        label_names (list): List of column names to scale.
+        scale_exp (list, optional): List of parameter exponents. 
+            Defaults to [1.0, 14.0, 14.0, 16.0, 0.0].
+
+    Returns:
+        pd.DataFrame: DataFrame of scaled target data.
     """
-    def scale_features_core(n, column):
-        column_values = data_table[column].values.reshape(-1, 1)
-        scaler_file = model_dir/'scalers'/f'xscaler_{n:02}.pkl'
+
+    # label_names = ['potential (V)', 'Ne (#/m^-3)', 'Ar+ (#/m^-3)', 'Nm (#/m^-3)', 'Te (eV)']
+    for i, column in enumerate(label_names):
+        data_table.update(data_table[column]/(10**scale_exp[i]))
+
+    return data_table
+
+
+def reverse_minmax(df: pd.DataFrame, columns: list, model_dir: Path):
+    """Reverse minmax scaling on data.
+
+    Model predictions are minmax-scaled to lie between 0 and 1. This function
+    reverses it for comparison with simulation data.
+
+    Args:
+        df (pd.DataFrame): _description_
+        columns (list): _description_
+        model_dir (Path): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    def reverse_minmax_core(values, n):
+        values = np.array([values]).reshape(1, -1)
+        scaler_file = model_dir/'scalers'/f'yscaler_{n:02}.pkl'
         with open(scaler_file, 'rb') as sf:
-            xscaler = pickle.load(sf)
-            xscaler.clip = False
-            return xscaler.transform(column_values)
+            yscaler = pickle.load(sf)
+            yscaler.clip = False
+            return yscaler.inverse_transform(values).item()
 
-    columns = [scale_features_core(n, column) for n, column in enumerate(data_table.columns, start=1)]
-    scaled_df = pd.DataFrame(np.hstack(columns))
+    columns = [df[column].apply(reverse_minmax_core, args=(n,)) for n, column in enumerate(df.columns)]
+    scaled_df = pd.concat(columns, axis=1)
 
-    return torch.FloatTensor(scaled_df.to_numpy())
+    return scaled_df
 
 
 def get_scores():
@@ -120,6 +156,7 @@ if __name__ == '__main__':
         minmax_y = metadata['is_target_scaled']
         lin = metadata['scaling']
         name = metadata['name']
+        scale_exp = metadata['parameter_exponents']
     else:
         print('Metadata unavailable: using defaults lin=True, minmax_y=True\n')
         lin = True
@@ -129,8 +166,10 @@ if __name__ == '__main__':
         print('\nLoaded model ' + name)
 
     features_tensor = scale_features(features, model_dir)
-    labels_tensor = scale_labels(labels, model_dir, lin)
+    targets = scale_targets(labels, label_names, scale_exp)
+
     model = MLP(len(feature_names), len(label_names))
     model.load_state_dict(torch.load(model_dir/f'{name}'))
-    model.eval()
-    # model
+    
+    model.eval()  # set model to eval mode
+    scaled_prediction = pd.DataFrame(model(features_tensor), columns=label_names)  # get the prediction