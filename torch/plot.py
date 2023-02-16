03cbe906530b95d5e712157472d489b102bae3ae
chao-zu <jcollado@observatory.ph>
2023-02-16 14:45:13 +0900

    Added plotting functions from data

--- torch/plot.py
+++ torch/plot.py
@@ -4,6 +4,130 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 
+# from data.py
+def get_cbar_range(param_col_label):
+    if param_col_label=='potential (V)':
+        cmin = 0.0
+        cmax = 210
+    elif param_col_label=='Ex (V/m)':
+        cmin = -150000
+        cmax =   75000
+    elif param_col_label=='Ey (V/m)':
+        cmin = -150000
+        cmax =  150000
+    elif param_col_label=='Ne (#/m^-3)':
+        cmin = 0.0
+        cmax = 1.6e16
+    elif param_col_label=='Ar+ (#/m^-3)':
+        cmin = 7.5e10
+        cmax = 1.6e16
+    elif param_col_label=='Nm (#/m^-3)':
+        cmin = 3.3e11
+        cmax = 8.9e18
+    elif param_col_label=='Te (eV)':
+        cmin =  0.0
+        cmax = 12.0
+    return cmin, cmax
+
+
+def get_cbar_range_300V_60Pa(param_col_label, lin=False): ## TODO: fix this
+    if lin:
+        if param_col_label=='potential (V)':
+            cmin =  0.0
+            cmax = 9.8
+        elif param_col_label=='Ex (V/m)':
+            cmin = -80000
+            cmax =  19000
+        elif param_col_label=='Ey (V/m)':
+            cmin = -68000
+            cmax =  72000
+        elif param_col_label=='Ne (#/m^-3)':
+            cmin = 0.0
+            cmax = 58
+        elif param_col_label=='Ar+ (#/m^-3)':
+            cmin = 4.5e-3
+            cmax = 58
+        elif param_col_label=='Nm (#/m^-3)':
+            cmin = 3.2e-3
+            cmax = 88
+        elif param_col_label=='Te (eV)':
+            cmin = 0.0
+            cmax = 6.0
+    else:
+        if param_col_label=='potential (V)':
+            cmin =  0.0
+            cmax = 98.0
+        elif param_col_label=='Ex (V/m)':
+            cmin = -80000
+            cmax =  19000
+        elif param_col_label=='Ey (V/m)':
+            cmin = -68000
+            cmax =  72000
+        elif param_col_label=='Ne (#/m^-3)':
+            cmin = 0.0
+            cmax = 5.8e15
+        elif param_col_label=='Ar+ (#/m^-3)':
+            cmin = 4.5e11
+            cmax = 5.8e15
+        elif param_col_label=='Nm (#/m^-3)':
+            cmin = 3.2e13
+            cmax = 8.8e17
+        elif param_col_label=='Te (eV)':
+            cmin = 0.0
+            cmax = 6.0
+    return cmin, cmax
+
+
+def draw_a_2D_graph(avg_data, param_col_label, file_path=None, set_cbar_range=True, c_only=False, lin=False):
+    # data
+    
+    units = {'potential (V)'    :' ($\mathrm{10 V}$)', 
+             'Ne (#/m^-3)'      :' ($10^{14}$ $\mathrm{m^{-3}}$)',
+             'Ar+ (#/m^-3)'     :' ($10^{14}$ $\mathrm{m^{-3}}$)', 
+             'Nm (#/m^-3)'      :' ($10^{16}$ $\mathrm{m^{-3}}$)',
+             'Te (eV)'          :' (eV)'}
+
+    
+    matplotlib.rcParams['font.family'] = 'Arial'
+    x = avg_data.X.values.reshape(-1,1)*100
+    y = avg_data.Y.values.reshape(-1,1)*100
+    z = avg_data[param_col_label].values.reshape(-1,1)
+    
+    cmap=plt.cm.viridis
+    
+    # settings for drawing
+    # plt.rcParams['font.family'] = 'serif'
+    fig = plt.figure(figsize=(3.3,7), dpi=200)
+    ax = fig.add_subplot(111)
+    ax.set_aspect('equal')
+    if set_cbar_range:
+        #cmin, cmax = get_cbar_range(param_col_label)
+        cmin, cmax = get_cbar_range_300V_60Pa(param_col_label, lin=lin)
+        sc = ax.scatter(x, y, c=z, cmap=cmap, alpha=0.5, s=2, linewidths=0, vmin=cmin, vmax=cmax)
+    else:
+        sc = ax.scatter(x, y, c=z, cmap=cmap, alpha=0.5, s=2, linewidths=0)
+    cbar = plt.colorbar(sc)
+    cbar.minorticks_off()
+    
+    if lin:
+        title = param_col_label.split(' ')[0] + units[param_col_label]
+    else:
+        title = param_col_label
+    
+    ax.set_title(title, fontsize=13)
+    ax.set_xlabel('r [cm]')
+    ax.set_ylabel('z [cm]')
+    ax.set_xlim(0, 21)
+    ax.set_ylim(0, 72)
+    fig.tight_layout()
+    
+    if file_path==None:
+        plt.show()
+    else:
+        fig.savefig(file_path)
+    plt.close('all')
+
+
 def save_history_graph(history, out_dir, param='mae'):
     # TODO: move to plot module
     matplotlib.rcParams['font.family'] = 'Arial'
