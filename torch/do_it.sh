a51f6755df6653fa59807e4b80e5c38efea44aba
chao-zu <astrowav@icloud.com>
2023-05-09 13:13:43 +0900

    Update do_it

--- torch/do_it.sh
+++ torch/do_it.sh
@@ -4,8 +4,6 @@
 
 echo -e "started on $(date)\n"
 
-python ./torch/create_model.py input_file.txt
-python ./torch/create_model.py input_file.txt
+python torch/MLP.py input_file.txt
 
 echo -e "\nfinished on $(date)\n"
-
