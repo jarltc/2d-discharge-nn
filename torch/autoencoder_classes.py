13a9408c4a7775d84be087d87dff0055c73551a2
chao-zu <astrowav@icloud.com>
2023-07-10 14:38:08 +0900

    Add symmetrical model

--- torch/autoencoder_classes.py
+++ torch/autoencoder_classes.py
@@ -86,6 +86,45 @@ class A300(nn.Module):
              decoded, 0, 0, 32, 32)
         return decoded  
 
+
+class A300s(nn.Module):
+    """Autoencoder using square images as inputs.
+    
+    Input sizes are (5, 32, 32).
+    """
+    def __init__(self) -> None:
+        super(A300s, self).__init__()
+        self.path = Path('/Users/jarl/2d-discharge-nn/created_models/autoencoder/32x32/A300s/A300s')
+        self.encoder = nn.Sequential(
+            nn.Conv2d(5, 10, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+
+            nn.Conv2d(10, 20, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+
+            nn.Conv2d(20, 20, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(20, 20, kernel_size=3, stride=2),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(20, 10, kernel_size=5, stride=2),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(10, 5, kernel_size=5, stride=2),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        decoded = torchvision.transforms.functional.crop(
+             decoded, 0, 0, 32, 32)
+        return decoded 
+    
+
 # 64x64 networks
 class A64_6(nn.Module):
     """Autoencoder using square images as inputs.
@@ -121,6 +160,51 @@ class A64_6(nn.Module):
 
             nn.Conv2d(10, 5, kernel_size=1, stride=1),
             nn.ReLU()
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        decoded = torchvision.transforms.functional.crop(
+            decoded, 0, 0, 64, 64)
+        return decoded
+
+
+class A64_6s(nn.Module):
+    """Autoencoder using square images as inputs.
+    
+    Input sizes are (5, 64, 64).
+    """
+    def __init__(self) -> None:
+        super(A64_6s, self).__init__()
+        self.path = Path('/Users/jarl/2d-discharge-nn/created_models/autoencoder/64x64/A64-6s/A64-6s')
+        self.resolution = 64
+        self.encoder = nn.Sequential(
+            nn.Conv2d(5, 10, kernel_size=5, stride=2, padding=2),
+            nn.ReLU(),
+
+            nn.Conv2d(10, 20, kernel_size=3, stride=2, padding=0),
+            nn.ReLU(),
+
+            nn.Conv2d(20, 40, kernel_size=1, stride=2, padding=0),
+            nn.ReLU(),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(40, 20, kernel_size=3, stride=2),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(20, 10, kernel_size=5, stride=2),
+            nn.ReLU(),
+
+            # nn.Conv2d(20, 20, kernel_size=(3, 3), stride=1, padding=1),
+            # nn.ReLU(),
+
+            nn.ConvTranspose2d(10, 5, kernel_size=5, stride=2),
+            nn.ReLU(),
+
+            # nn.Conv2d(10, 5, kernel_size=1, stride=1),
+            # nn.ReLU()
         )
 
     def forward(self, x):
