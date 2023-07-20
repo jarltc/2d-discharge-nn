e8e4aed5ff343d3af9b0db9aa0b643d8069b7d9f
chao-zu <astrowav@icloud.com>
2023-07-20 12:39:18 +0900

    how did I make the test set nc file

---
+++ torch/search.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+search_string="test_set.nc"
+
+# Loop through all the files in the current directory
+for file in *; do
+    if [ -f "$file" ]; then  # Check if the current item is a file
+        # Use grep to search for the string in the file
+        if grep -q "$search_string" "$file"; then
+            echo "Found '$search_string' in file: $file"
+        fi
+    fi
+done
