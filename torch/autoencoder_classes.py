351ee7399fe4df721edd64ba37d748cd829b38ff
chao-zu <astrowav@icloud.com>
2023-08-01 00:18:57 +0900

    designed some random model based on a64-6

--- torch/autoencoder_classes.py
+++ torch/autoencoder_classes.py
@@ -126,6 +126,53 @@ class A300s(nn.Module):
     
 
 # 64x64 networks
+class A64_7(nn.Module):
+    """A64_6 with fixed padding of 1 and an extra convolutional layer at the decoder (idk why really)
+    
+    Input sizes are (5, 64, 64).
+    """
+    def __init__(self) -> None:
+        super(A64_7, self).__init__()
+        self.path = Path('/Users/jarl/2d-discharge-nn/created_models/autoencoder/64x64/A64-7/A64-7')
+        self.encoder = nn.Sequential(
+            nn.Conv2d(5, 10, kernel_size=5, stride=2, padding=1),
+            nn.ReLU(),
+
+            nn.Conv2d(10, 20, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+
+            nn.Conv2d(20, 40, kernel_size=1, stride=1, padding=1),
+            nn.ReLU(),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(40, 40, kernel_size=3, stride=2),
+            nn.ReLU(),
+
+            nn.Conv2d(40, 40, kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(40, 20, kernel_size=5, stride=2),
+            nn.ReLU(),
+
+            nn.Conv2d(20, 20, kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(20, 10, kernel_size=5, stride=2),
+            nn.ReLU(),
+
+            nn.Conv2d(10, 5, kernel_size=1, stride=1),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        decoded = torchvision.transforms.functional.crop(
+            decoded, 0, 0, 64, 64)
+        return decoded
+
+
 class A64_6(nn.Module):
     """Autoencoder using square images as inputs.
     
