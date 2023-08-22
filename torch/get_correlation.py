54d91f014d53fa2abf37dcfa755f5b856bb770b7
chao-zu <astrowav@icloud.com>
2023-08-22 11:06:34 +0800

    this still doesn't work

--- torch/get_correlation.py
+++ torch/get_correlation.py
@@ -1,4 +1,4 @@
-from plot import ae_correlation
+from plot import ae_correlation, plot_comparison_ae
 from image_data_helpers import get_data, nc_data
 from autoencoder_classes import A64_6, A300
 from mlp_classes import MLP
@@ -39,10 +39,10 @@ if __name__ == '__main__':
         encodedz = 8
         encoded_size = encodedx*encodedy*encodedz 
 
-        mlp = MLP(2, encoded_size, dropout_prob=0.5)
-        mlp.load_state_dict(torch.load(mlp.path64))
-        mlp.to(device)
         out_dir = Path('/Users/jarl/2d-discharge-nn/created_models/conditional_autoencoder/64x64/A64g')
+        mlp = MLP(2, encoded_size, dropout_prob=0.5)
+        mlp.load_state_dict(torch.load(out_dir/'A64g'))
+        mlp.to(device)
 
     elif resolution == 32:
         ae_model = A300()
@@ -65,17 +65,14 @@ if __name__ == '__main__':
     sV, sP = label_minmax(300, 60)
     labels = torch.tensor([sV, sP], device=device, dtype=torch.float32)
 
-    # get predictions
-    # not needed ?
-    # test_data = torch.tensor(test_data, dtype=torch.float32, device=device)
-
     mlp.eval()
     ae_model.eval()
     with torch.no_grad():
-        fake_encoding = mlp(labels)
-        fake_encoding = fake_encoding.reshape(1, encodedx, encodedy, encodedz)
+        fake_encoding = mlp(labels).reshape(1, encodedx, encodedy, encodedz)
         decoded = ae_model.decoder(fake_encoding)
         decoded = torchvision.transforms.functional.crop(
              decoded, 0, 0, resolution, resolution)
 
-    r2 = ae_correlation(test_data, decoded, out_dir)
+    r2 = ae_correlation(test_data, decoded, out_dir=out_dir)  # something wrong with this??
+    plot_comparison_ae(test_data, fake_encoding, ae_model, 
+                       out_dir=out_dir, is_square=True, resolution=resolution)
