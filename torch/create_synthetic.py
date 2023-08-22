4fa00846566196c61e5669ecd7a92202c4a0189c
chao-zu <astrowav@icloud.com>
2023-08-22 16:28:30 +0800

    use multithreading with concurrent.futures

--- torch/create_synthetic.py
+++ torch/create_synthetic.py
@@ -3,19 +3,23 @@
 created by jarl on 22 Aug 2023
 """
 
-import numpy as np
 from pathlib import Path
-import xarray as xr
-from image_data_helpers import get_dataset
 import itertools
-import multiprocessing as mp
+import concurrent.futures
+import time
+
+import numpy as np
+import xarray as xr
+from dask.diagnostics import ProgressBar
+
+from image_data_helpers import get_dataset
 
 # list of transforms
 # average of any two datasets
 # vertical flip
 # horizontal flip
 
-def average_pair(a, b):
+def average_pair(a, b, ds):
     """Process the average array between two pairs a and b.
 
     Args:
@@ -25,9 +29,10 @@ def average_pair(a, b):
     Returns:
         _type_: _description_
     """
-    global variable_names
-    global y 
-    global x
+    variable_names = list(ds.keys())
+    
+    y = ds.y.values
+    x = ds.x.values
 
     va, pa = a
     vb, pb = b
@@ -47,27 +52,42 @@ def average_pair(a, b):
     
     return xr.merge(channel_data_arrays)
 
-ncfile = Path('/Users/jarl/2d-discharge-nn/data/interpolation_datasets/rec-interpolation2.nc')
-ds = xr.open_dataset(ncfile)
 
-v_list = list(ds.V.values)
-p_list = list(ds.P.values)
-variable_names = list(ds.keys())
-x = ds.x.values
-y = ds.y.values
+if __name__ == "__main__":
+    ncfile = Path('/Users/jarl/2d-discharge-nn/data/interpolation_datasets/rec-interpolation2.nc')
+    ds = xr.open_dataset(ncfile)
 
-vps = [(v, p) for v in v_list for p in p_list]  # get all pairs of V and P
+    v_list = list(ds.V.values)
+    p_list = list(ds.P.values)
+    variable_names = list(ds.keys())
 
-# create a list of combinations of pairs of v, p = ((v1, p1), (v2, p2))
-averaged_dataset = xr.concat([average_pair(a,b) for a, b in list(itertools.combinations(vps, 2))],
-                             dim='image')
+    vps = [(v, p) for v in v_list for p in p_list]  # get all pairs of V and P
 
-# write to file
-out_dir = ncfile.parent
-name = 'synthetic_averaged'
-out_file = out_dir/f'{name}.nc'
-write_job = ds.to_netcdf(out_file, compute=False)
-from dask.diagnostics import ProgressBar
-with ProgressBar():
-    print(f'Writing to {out_file}')
-    write_job.compute()
+    # add a combination of ((v1, p1), (v2, p2)) to the input queue
+    start_time = time.perf_counter()
+    pairs = list(itertools.combinations(vps, 2))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        # submit tasks to the executor
+        future_to_task = {executor.submit(average_pair, pair[0], pair[1], ds): pair for pair in pairs}
+
+        # wait for tasks to complete and retrieve results
+        results = []
+        for future in concurrent.futures.as_completed(future_to_task):
+            task = future_to_task[future]
+            results.append(future.result())
+
+    # combine images (xr.datasets) into one big dataset
+    averaged_dataset = xr.concat(results, dim='image')
+    end_time = time.perf_counter()
+
+    print(f'Processing took {end_time-start_time} s with multithreading\n')
+
+    # write to file
+    out_dir = ncfile.parent
+    name = 'synthetic_averaged'
+    out_file = out_dir/f'{name}.nc'
+    write_job = averaged_dataset.to_netcdf(out_file, compute=False)
+
+    with ProgressBar():
+        print(f'Writing to {out_file}')
+        write_job.compute()
