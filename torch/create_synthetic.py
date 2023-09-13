8d62510dc5b6f202bcf7c4d2f1169721feddd2b4
chao-zu <astrowav@icloud.com>
2023-09-13 11:36:40 +0900

    Create downscaled, square datasets

--- torch/create_synthetic.py
+++ torch/create_synthetic.py
@@ -16,14 +16,14 @@ import numpy as np
 import xarray as xr
 from dask.diagnostics import ProgressBar
 
-from image_data_helpers import get_dataset
+from image_data_helpers import get_dataset, crop, downscale, minmax_scale
 
 # list of transforms
 # average of any two datasets
 # vertical flip
 # horizontal flip
 
-def average_pair(a, b, ds):
+def average_pair(a, b, ds, resolution=None):
     """Process the average array between two pairs a and b.
 
     Args:
@@ -35,8 +35,13 @@ def average_pair(a, b, ds):
     """
     variable_names = list(ds.keys())
     
-    y = ds.y.values
-    x = ds.x.values
+    # convert coordinates based on scaling if resolution provided
+    if resolution is not None:
+        y = np.linspace(0, max(ds.y.values), resolution)
+        x = np.linspace(0, max(ds.x.values), resolution)
+    else:
+        y = ds.y.values
+        x = ds.x.values
 
     va, pa = a
     vb, pb = b
@@ -47,9 +52,18 @@ def average_pair(a, b, ds):
     # dataA and B have shapes of (channels, height, width), we need to get the 
     # average between the two that maintains the same shape
     z = (dataA + dataB) / 2.0  # numpy arrays (chanels, height, width)
+
+    # crop to square
+    if resolution is not None:
+        cropped = crop(z)
+        # downscale 
+        dscaled = downscale(cropped, resolution)
+    else:
+        dscaled = z
+    scaled = minmax_scale(dscaled, ds)
     
     # process into xarray dataset
-    channel_data_arrays = [xr.DataArray(z[i], dims=['y', 'x'],
+    channel_data_arrays = [xr.DataArray(scaled[i], dims=['y', 'x'],
                                         coords={'y':y, 'x':x}, 
                                         name=variable_names[i]) 
                                         for i in range(z.shape[0])]
@@ -65,14 +79,20 @@ if __name__ == "__main__":
     p_list = list(ds.P.values)
     variable_names = list(ds.keys())
 
+    resolution = 64
+    if resolution is not None:
+        square = True
+    suffix = f's{resolution}'
+
     vps = [(v, p) for v in v_list for p in p_list]  # get all pairs of V and P
 
     # add a combination of ((v1, p1), (v2, p2)) to the input queue
+    print('job started')
     start_time = time.perf_counter()
     pairs = list(itertools.combinations(vps, 2))
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:  # Future objects represent the result of an asynchronous task.
         # submit tasks to the executor, performs average_pair() on a pair in the list initialized earlier.
-        future_to_task = {executor.submit(average_pair, pair[0], pair[1], ds): pair for pair in pairs}  # creates a dictionary of future: task, where a task serves
+        future_to_task = {executor.submit(average_pair, pair[0], pair[1], ds, resolution): pair for pair in pairs}  # creates a dictionary of future: task, where a task serves
                                                                                                         # as the key to a future.
 
         # wait for tasks to complete and retrieve results
@@ -89,7 +109,10 @@ if __name__ == "__main__":
     # write to file
     out_dir = ncfile.parent
     name = 'synthetic_averaged'
-    out_file = out_dir/f'{name}.nc'
+    if square:
+        out_file = out_dir/f'{name}_s{resolution}.nc'
+    else:
+        out_file = out_dir/f'{name}.nc'
     write_job = averaged_dataset.to_netcdf(out_file, compute=False)
 
     with ProgressBar():
