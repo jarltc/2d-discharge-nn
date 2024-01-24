ba8ee747dc4c82d9ea7063dec72eeb8be3bbdede
chao-zu <astrowav@icloud.com>
2024-01-24 17:05:03 +0900

    split out functionality of previous sep_v2 plotting code

--- torch/cae_regr.py
+++ torch/cae_regr.py
@@ -12,27 +12,21 @@ import matplotlib.pyplot as plt
 
 import cv2
 import numpy as np
-import pandas as pd
-import xarray as xr
-from tqdm import tqdm
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
 from torchvision.transforms.functional import crop
-from torch.utils.data import TensorDataset, DataLoader
-from torchinfo import summary
+import torch.utils.benchmark as benchmark
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 from data_helpers import ImageDataset
-from plot import plot_comparison_ae, save_history_graph, ae_correlation, image_slices, delta, sep_comparison_ae_v2
+from plot import plot_comparison_ae, save_history_graph, ae_correlation, image_slices, delta, image_compare
 import autoencoder_classes
 import mlp_classes
 from image_data_helpers import get_data
+from data_helpers import mse
 
 # define model TODO: construct following input file/specification list
 
@@ -69,12 +63,33 @@ def write_metadata(out_dir):  # TODO: move to data module
 
     file = out_dir/'regr_metadata.txt'
     with open(file, 'w') as f:
-        print(model, file=f)
+        print(autoencoder, file=f)
         f.write(f'Resolution: {resolution}\n')
-        f.write(f'Evaluation time: {(eval_time*1e-6):.2f} ms\n')
-        f.write(f'Scores (MSE): {scores}\n')
+        f.write(f'Evaluation time: {eval_time:.2f} ms\n')
+        f.write(f'Scores (MSE): {mse_scores}\n')
         f.write(f'Scores (r2): {r2}\n')
         f.write('\n***** end of file *****')
+
+
+def imageMLP_eval(test_label, mlp, autoencoder):
+    """Model evaluation for ImageMLP.
+
+    Args:
+        mlp (torch.nn.Module): ImageMLP.
+        autoencoder (torch.nn.Module): Autoencoder to be used to produce the output image.
+        test_label (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    global encodedx, encodedy, encodedz
+
+    with torch.no_grad():  # disable gradients computation
+        fake_encoding = mlp(torch.tensor(test_label, device=device, dtype=torch.float32))\
+            .reshape(1, encodedx, encodedy, encodedz)  # mps does not support float64
+        decoded = autoencoder.decoder(fake_encoding).cpu().numpy()[:, :, :resolution, :resolution]
+
+    return fake_encoding, decoded
 
 
 def speedtest(test_label: torch.tensor):
@@ -92,6 +107,12 @@ def speedtest(test_label: torch.tensor):
                             globals={'test_label':test_label, 'mlp':mlp, 'autoencoder':autoencoder})
 
     return timer.timeit(100)
+
+
+def scores(reference, prediction):
+    # record scores
+    return [mse(reference[0, i, :, :], prediction[0, i, :, :]) for i in range(5)]
+
 
 if __name__ == '__main__':
     # set metal backend (apple socs)
@@ -113,6 +134,7 @@ if __name__ == '__main__':
     encodedx, encodedy, encodedz = autoencoder.encoded_shape
     encoded_size = encodedx * encodedy * encodedz
 
+    mlp = mlp_classes.MLP4(2, encoded_size, dropout_prob=0.5)
 
     out_dir = mlp_dir.parents[0]
     if not out_dir.exists():
@@ -122,11 +144,23 @@ if __name__ == '__main__':
     _, test = get_data((300, 60), resolution=resolution, square=is_square, labeled=True)
     test_image, test_label = test
 
-    model.load_state_dict(torch.load(ae_dir))  # use path directly to model
+    autoencoder.load_state_dict(torch.load(ae_dir))  # use path directly to model
     mlp.load_state_dict(torch.load(mlp_dir))  # use path directly to model
-    model.to(device)  # move model to gpu
+    
+    # move models to gpu
+    autoencoder.to(device)
     mlp.to(device)
-    model.eval()
+
+    # place models in evaluation mode
     mlp.eval()
+    autoencoder.eval()
+    
+    fake_encoding, prediction = imageMLP_eval(test_label, mlp, autoencoder)
+
     eval_time = speedtest(test_label).median * 1e3  # seconds to ms
+    fig = image_compare(test_image, prediction, out_dir=out_dir, is_square=is_square, cmap='viridis', unscale=True)
+    mse_scores = scores(test_image, prediction)
+    r2 = ae_correlation(test_image, prediction, out_dir)
+    # slices = image_slices(test_image, decoded, out_dir=out_dir, cmap='viridis')
+    # delta_fig = delta(test_image, decoded, out_dir=out_dir, is_square=True)
     write_metadata(out_dir)
