a6d867400a7e626af4ecfaad8d8859c1fda33633
Jarl <collado@ppl.eng.osaka-u.ac.jp>
2024-04-07 14:14:08 +0200

    init: imagemlp

---
+++ remote/imageMLP.py
@@ -0,0 +1,203 @@
+"""
+Conditional autoencoder to reproduce 2d plasma profiles from a pair of V, P
+
+* Load encoder
+* Load MLP architecture
+
+"""
+
+import os
+import time
+import pickle
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+import cv2
+import numpy as np
+import pandas as pd
+import xarray as xr
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision.transforms.functional import crop
+from torch.utils.data import TensorDataset, DataLoader
+from torchinfo import summary
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+from data_helpers import ImageDataset, train2db, set_device
+from plot import plot_comparison_ae, save_history_graph, ae_correlation
+import autoencoder_classes
+import mlp_classes
+from image_data_helpers import get_data
+
+
+def resize(data: np.ndarray, scale=64) -> np.ndarray:
+    """Resize square images to 64x64 resolution by downscaling.
+
+    Args:
+        data (np.ndarray): Input data.
+
+    Returns:
+        np.ndarray: Downscaled input data.
+    """
+
+    data = np.stack([cv2.resize((np.moveaxis(image, 0, -1)), (scale, scale)) for image in data]) 
+    return np.moveaxis(data, -1, 1)  # revert initial moveaxis
+
+
+def normalize_test(dataset:np.ndarray, scalers:dict):
+    normalized_variables = []
+
+    for i, var in enumerate(['pot', 'ne', 'ni', 'nm', 'te']):
+        x = dataset[:, i, :, :]
+        xMin, xMax = scalers[var]
+        scaledx = (x-xMin) / (xMax-xMin)  # shape: (31, x, x)
+        normalized_variables.append(scaledx)
+    
+    # shape: (5, 31, x, x)
+    normalized_dataset = np.moveaxis(np.stack(normalized_variables), 0, 1)  # shape: (31, 5, x, x)
+    return normalized_dataset
+
+
+def write_metadata_ae(out_dir):  # TODO: move to data module
+    # if is_square:
+    #     in_size = (1, 5, 200, 200)
+    # else:
+    #     in_size = (1, 5, 707, 200)
+
+    in_size = batch_data[0].size()  # testing
+
+    # save model structure
+    file = out_dir/'train_log.txt'
+    with open(file, 'w') as f:
+        if message is not None:
+            f.write(f'Message: {message}\n')
+        f.write(f'Model {name}\n')
+        print(summary(ae_model, input_size=in_size), file=f)
+        print("\n", file=f)
+        print(ae_model, file=f)
+        print(summary(mlp, input_size=(1, 2)), file=f)
+        print("\n")
+        f.write(f'\nEpochs: {epochs}\n')
+        f.write(f'Learning rate: {learning_rate}\n')
+        f.write(f'Resolution: {resolution}\n')
+        f.write(f'Train time: {(train_end-train_start):.2f} s\n')
+        # f.write(
+        #     f'Average time per epoch: {np.array(epoch_times).mean():.2f} s\n')
+        f.write(f'Evaluation time: {(eval_time/1e6):.2f} ms\n')
+        f.write(f'Scores (MSE): {scores}\n')
+        f.write('\n***** end of file *****')
+
+
+if __name__ == '__main__':
+    # set metal backend (apple socs)
+    device = set_device()
+    
+    name = input("Model name: ") or "CAE_test"
+    message = input("Leave a note?: ") or None
+    root = Path.cwd()
+
+    # ----- #
+    resolution = 64
+    if resolution == 32:
+        ae_model = autoencoder_classes.A300()
+        encodedx = 20
+        encodedy = encodedz = 4
+    elif resolution == 64:
+        ae_model = autoencoder_classes.A64_8()
+        encodedx = 40
+        encodedy = encodedz = 8
+    
+    encoded_size = encodedx*encodedy*encodedz
+
+    ##### things to change #####
+    model_dir = root/'created_models'/'autoencoder'/'64x64'/'A64-8'/'A64-8'  # path to autoencoder model
+    
+    # ----- #
+    epochs = 500
+    learning_rate = 1e-3
+    dropout_prob = 0.5
+    # ----- #
+    mlp = mlp_classes.MLP4(2, encoded_size, dropout_prob=dropout_prob)
+    mlp.to(device)
+    
+    train, test = get_data((300, 60), resolution=resolution, labeled=True)
+    train_images, train_labels = train
+    test_image, test_label = test
+
+    out_dir = root/'created_models'/'conditional_autoencoder'/name
+    if not out_dir.exists():
+        out_dir.mkdir(parents=True)
+
+    dataset = TensorDataset(torch.tensor(train_images, device=device, dtype=torch.float32),
+                            torch.tensor(train_labels, device=device, dtype=torch.float32))
+    trainloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    
+    # load autoencoder model
+    ae_model.load_state_dict(torch.load(model_dir))
+    ae_model.to(device)
+    ae_model.eval()  # inference mode, disables training
+    
+    #### train MLP ####
+    optimizer = optim.Adam(mlp.parameters(), lr=learning_rate)
+    criterion = nn.MSELoss()
+    epoch_loss = []
+
+    # begin training MLP
+    print("Training MLP...\r", end="")
+    train_start = time.time()
+    loop = tqdm(range(epochs), desc='Training...', unit='epoch', colour='#7dc4e4')
+
+    for epoch in loop:
+        running_loss = 0.0  # record losses
+
+        for i, batch_data in enumerate(trainloader):
+            image, labels = batch_data  # feed in images and labels (V, P)
+
+            optimizer.zero_grad()  # reset gradients
+
+            encoding = mlp(labels).reshape(1, encodedx, encodedy, encodedz)  # forward pass, get mlp prediction from (v, p) then reshape
+            output = ae_model.decoder(encoding)  # get output image from decoder
+            output = torchvision.transforms.functional.crop(output, 0, 0, resolution, resolution)  # crop to match shape
+
+            loss = criterion(output, image)  # get the loss between input image and model output
+            loss.backward()  # backward propagation
+            optimizer.step()  # apply changes to network
+
+            # print statistics
+            loop.set_description(f"Epoch {epoch+1}/{epochs}")
+            running_loss += loss.item()
+
+        epoch_loss.append(loss.item())
+        if (epoch+1) % 10 == 0:
+            # save model every 10 epochs (so i dont lose all training progress in case i do something unwise)
+            torch.save(ae_model.state_dict(), out_dir/f'{name}')
+
+    print("\33[2KMLP training complete!")
+    train_end = time.time()
+    torch.save(mlp.state_dict(), out_dir/f'{name}')
+    save_history_graph(epoch_loss, out_dir)
+
+    mlp.eval()
+
+    # construct a fake encoding from a pair of (V, P) and reshape to the desired dimensions
+    with torch.no_grad():
+        fake_encoding = mlp(torch.tensor(test_label, device=device, dtype=torch.float32))  # mps does not support float64
+        # reshape encoding from (1, xyz) to (1, x, y, z)
+        fake_encoding = fake_encoding.reshape(1, encodedx, encodedy, encodedz)
+        decoded = ae_model.decoder(fake_encoding)
+        decoded = torchvision.transforms.functional.crop(decoded, 0, 0, resolution, resolution)
+
+    # add resolution=64 for larger images
+    eval_time, scores = plot_comparison_ae(test_image, fake_encoding, ae_model, 
+                                           out_dir=out_dir, is_square=True)
+    write_metadata_ae(out_dir)
+    ae_correlation(test_image, decoded, out_dir, minmax=False)
+    # train2db(out_dir, name, epochs, image_ds.v_excluded, image_ds.p_excluded, resolution, typ='mlp')
---
+++ remote/mlp_classes.py
@@ -0,0 +1,208 @@
+"""ImageMLP classes"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from pathlib import Path
+
+root = Path.cwd()
+model_dir = root/'created_models'/'conditional_autoencoder'
+
+# 32 x 32
+class MLP(nn.Module):
+    """MLP to recreate encodings from a pair of V and P.
+    """
+    def __init__(self, input_size, output_size, dropout_prob) -> None:
+        super(MLP, self).__init__()
+        self.path64 = model_dir/'64x64'/'A64g'/'A64g'
+        self.path32 = model_dir/'32x32'/'A32g'/'A32g'
+        self.input_size = input_size
+        self.output_size = output_size
+        self.fc1 = nn.Linear(input_size, 256)
+        self.fc2 = nn.Linear(256, 512)
+        self.fc3 = nn.Linear(512, 1024)
+        self.fc4 = nn.Linear(1024, output_size)  # for output size of 2560, we halve the neurons per layer
+        self.dropout = nn.Dropout(dropout_prob)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc3(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc4(x)
+        output = F.relu(x)
+
+        return output
+
+
+class MLP1(nn.Module):
+    """MLP to recreate encodings from a pair of V and P.
+
+    dont use this yet lol
+    """
+    def __init__(self, input_size, output_size, dropout_prob) -> None:
+        super(MLP1, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.input = nn.Linear(input_size, 512)
+        self.fc1 = nn.Linear(512, 512)
+        
+        self.output = nn.Linear(512, output_size)
+        self.dropout = nn.Dropout(dropout_prob)
+
+    def forward(self, x):
+        x = F.relu(self.input(x))
+
+        for _ in range(5):
+            x = self.fc1(x)
+            x = F.relu(x)
+
+        output = F.relu(self.output(x))
+
+        return output
+
+
+# 64 x 64
+class MLP64(nn.Module):
+    """MLP to recreate encodings from a pair of V and P.
+    """
+    def __init__(self, input_size, output_size, dropout_prob) -> None:
+        super(MLP64, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.fc1 = nn.Linear(input_size, 80)
+        self.fc2 = nn.Linear(80, 160)
+        self.fc3 = nn.Linear(160, 320)
+        self.fc4 = nn.Linear(320, 640)
+        self.fc5 = nn.Linear(640, 1280)
+        self.fc6 = nn.Linear(1280, output_size)  # for output size of 2560, we halve the neurons per layer
+        self.dropout = nn.Dropout(dropout_prob)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc3(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc4(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc5(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc6(x)
+        output = F.relu(x)
+
+        return output
+    
+
+class MLP2(nn.Module):
+    """MLP with no dropout
+    """
+    def __init__(self, input_size, output_size, dropout_prob) -> None:
+        super(MLP2, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.fc1 = nn.Linear(input_size, 40)
+        self.fc2 = nn.Linear(40, 80)
+        self.fc3 = nn.Linear(80, 160)
+        self.fc4 = nn.Linear(160, 320)
+        self.fc5 = nn.Linear(320, 640)
+        self.fc6 = nn.Linear(640, 1280)
+        self.fc7 = nn.Linear(1280, output_size)  # for output size of 2560, we halve the neurons per layer
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        x = self.fc2(x)
+        x = F.relu(x)
+
+        x = self.fc3(x)
+        x = F.relu(x)
+
+        x = self.fc4(x)
+        x = F.relu(x)
+
+        x = self.fc5(x)
+        x = F.relu(x)
+
+        x = self.fc6(x)
+        x = F.relu(x)
+
+        x = self.fc7(x)
+        output = F.relu(x)
+
+        return output
+    
+
+class MLP3(nn.Module):
+    """MLP1 with more nodes?
+    """
+    def __init__(self, input_size, output_size, dropout_prob) -> None:
+        super(MLP3, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.input = nn.Linear(input_size, 1024)
+        self.fc1 = nn.Linear(1024, 1024)
+        
+        self.output = nn.Linear(1024, output_size)
+        self.dropout = nn.Dropout(dropout_prob)
+
+    def forward(self, x):
+        x = F.relu(self.input(x))
+
+        for _ in range(6):
+            x = self.fc1(x)
+            x = F.relu(x)
+
+        output = F.relu(self.output(x))
+
+        return output
+    
+
+class MLP4(nn.Module):
+    """MLP3 with LESS nodes?
+    """
+    def __init__(self, input_size, output_size, dropout_prob) -> None:
+        super(MLP4, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.input = nn.Linear(input_size, 20)
+
+        self.fc1 = nn.Linear(20, 40)
+        self.fc2 = nn.Linear(40, 80)
+        self.fc3 = nn.Linear(80, 100)
+        self.fc4 = nn.Linear(100, 120)
+        
+        self.output = nn.Linear(120, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.input(x))
+
+        # hidden layers
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+
+        output = F.relu(self.output(x))
+
+        return output
